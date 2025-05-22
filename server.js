1ad560df85a2dc19f66630c080f45c08300380e0
ithalloantunes <ithallo36993@gmail.com>
2025-05-22 16:11:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://projeto-estoque.onrender.com'], // Atualize com seu domínio do Render
   credentials: true
 }));
 app.use(express.json());
